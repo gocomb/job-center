f6e9d99bd3ac3bf4a1833a13bae7c8deec9fb4e8
root <li.ym@inspur.com>
2017-07-07 15:14:49 +0800

    20170707

--- src/message/types.go
+++ src/message/types.go
@@ -1,1 +1,23 @@
 package message
+
+import "util/logger"
+
+func init() {
+
+}
+
+type Interface interface {
+	Info(...interface{}) func(parameter ...interface{})
+}
+
+type methodMessage struct {
+}
+
+
+//消息文本，输出消息信息，返回文本数组
+func (c *methodMessage) Info(m ...interface{})interface{} {
+	s := new(logger.NewLog)
+	s.LogRegister(logger.LevelInfo)
+	s.SetInfo(m...)
+	return m
+}
--- src/message/types_test.go
+++ src/message/types_test.go
@@ -1,1 +1,12 @@
 package message
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestNewMessage_Info(t *testing.T) {
+	c:=new(NewMessage)
+	bb:=c.Info("sdf","asdasdas")
+	fmt.Println(bb)
+}
